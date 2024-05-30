42b84c7f73a4169f2161e431698a4758a1417dc7
AzatE <marateni@gmail.com>
2024-05-30 20:48:30 +0500

    Добавляет фрагменты интерфейса (без связи с бд)

---
+++ main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, Request, Header, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from typing import Annotated
+
+from sqlalchemy.orm import Session
+
+from sql.database import Session, engine
+import sql.models as models
+from sql.database_func import select_values_from_tables, set_values_from_mail_log
+from backend.parse_log import parse_log_file
+
+# models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+# Dependency
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/index/", response_class=HTMLResponse)
+async def index(
+    request: Request,
+    hx_request: Annotated[str | None, Header()] = None,
+    db: Session = Depends(get_db),
+    page: int = 1,
+):
+    print("it's working")
+    # COUNT_ROWS = 2
+    # offset = (page - 1) * COUNT_ROWS
+    # print(f"page = {page}, offset = {offset}")
+    #
+    # records = db.query(models.Film).offset(offset).limit(COUNT_ROWS)
+    records = [
+        {'time': '10-11-2023', 'text': 'example1'},
+        {'time': '11-11-2023', 'text': 'example2'},
+        {'time': '12-11-2023', 'text': 'example3'},
+    ]
+    page = 1
+    context = {"request": request, "records": records, "page": page}
+    if hx_request:
+        return templates.TemplateResponse("table.html", context)
+    return templates.TemplateResponse(name="index.html", context=context)
+
+
+if __name__ == "__main__":
+    print("create session")
+    current_session = Session()
+    """
+    
+    print("parse file")
+    result_from_log = parse_log_file("data/out")
+    print("into base")
+    set_values_from_mail_log(current_session, result_from_log )
+    print("select_values_from_tables")
+    """
+
+    result_from_select = select_values_from_tables(current_session, "kuxanwyqalsszn@gmail.com")
+    for row in result_from_select:
+        row_as_dict = row._mapping
+        print(row_as_dict)
+
+
