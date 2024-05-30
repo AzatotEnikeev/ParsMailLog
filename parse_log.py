60a651e9380018545352213558a07022c16237ef
AzatE <marateni@gmail.com>
2024-05-30 14:22:36 +0500

    refactor using constants.py in parse_log

--- parse_log.py
+++ parse_log.py
@@ -1,6 +1,6 @@
 from mail_log_class import MailLogClassInfo
 import database
-import constants
+from constants import COL_LOG_DATE, COL_LOG_TIME, COL_LOG_KEY_ID, COL_LOG_FLAG, COL_LOG_ADDRESS, COL_LOG_ANOTHER_INFO, COL_LOG_END
 import re
 
 def parse_log_file(file_name: str, order: list):
@@ -11,39 +11,39 @@ def parse_log_file(file_name: str, order: list):
         details = [x.rstrip() for x in details_line]
         mail_log = MailLogClassInfo()
 
-        if details[constants.COL_LOG_FLAG] == '<=':
+        if details[COL_LOG_FLAG] == '<=':
             if re.findall(r'id=\w*',line):
-                mail_log.date = details[constants.COL_LOG_DATE]
-                mail_log.time = details[constants.COL_LOG_TIME]
-                mail_log.flag = details[constants.COL_LOG_FLAG]
-                mail_log.id_self = details[constants.COL_LOG_KEY_ID]
-                mail_log.mail_adress = details[constants.COL_LOG_ADDRESS]
-                mail_log.another_information = " ".join(details[constants.COL_LOG_ANOTHER_INFO:])
+                mail_log.date = details[COL_LOG_DATE]
+                mail_log.time = details[COL_LOG_TIME]
+                mail_log.flag = details[COL_LOG_FLAG]
+                mail_log.id_self = details[COL_LOG_KEY_ID]
+                mail_log.mail_adress = details[COL_LOG_ADDRESS]
+                mail_log.another_information = " ".join(details[COL_LOG_ANOTHER_INFO:])
                 mail_log.id =  re.findall(r'id=(\S+)',line)
             else:
                 continue
 
-        elif details[constants.COL_LOG_FLAG] in ['=>', '->', '**', '==']:
+        elif details[COL_LOG_FLAG] in ['=>', '->', '**', '==']:
 
-            if len(details) >= 4 and details[constants.COL_LOG_ADDRESS] == ":blackhole:":
-                mail_log.date = details[constants.COL_LOG_DATE]
-                mail_log.time = details[constants.COL_LOG_TIME]
-                mail_log.flag = details[constants.COL_LOG_FLAG]
-                mail_log.id_self = details[constants.COL_LOG_KEY_ID]
-                mail_log.mail_adress = details[constants.COL_LOG_ADDRESS] + details[constants.COL_LOG_ANOTHER_INFO]
-                mail_log.another_information = " ".join(details[constants.COL_LOG_END:])
+            if len(details) >= 4 and details[COL_LOG_ADDRESS] == ":blackhole:":
+                mail_log.date = details[COL_LOG_DATE]
+                mail_log.time = details[COL_LOG_TIME]
+                mail_log.flag = details[COL_LOG_FLAG]
+                mail_log.id_self = details[COL_LOG_KEY_ID]
+                mail_log.mail_adress = details[COL_LOG_ADDRESS] + details[COL_LOG_ANOTHER_INFO]
+                mail_log.another_information = " ".join(details[COL_LOG_END:])
             else:
-                mail_log.date = details[constants.COL_LOG_DATE]
-                mail_log.time = details[constants.COL_LOG_TIME]
-                mail_log.flag = details[constants.COL_LOG_FLAG]
-                mail_log.id_self = details[constants.COL_LOG_KEY_ID]
-                mail_log.mail_adress = details[constants.COL_LOG_ADDRESS]
-                mail_log.another_information = " ".join(details[constants.COL_LOG_ANOTHER_INFO:])
+                mail_log.date = details[COL_LOG_DATE]
+                mail_log.time = details[COL_LOG_TIME]
+                mail_log.flag = details[COL_LOG_FLAG]
+                mail_log.id_self = details[COL_LOG_KEY_ID]
+                mail_log.mail_adress = details[COL_LOG_ADDRESS]
+                mail_log.another_information = " ".join(details[COL_LOG_ANOTHER_INFO:])
         else:
-            mail_log.date = details[constants.COL_LOG_DATE]
-            mail_log.time = details[constants.COL_LOG_TIME]
-            mail_log.id_self = details[constants.COL_LOG_KEY_ID]
-            mail_log.another_information = " ".join(details[constants.COL_LOG_FLAG:])
+            mail_log.date = details[COL_LOG_DATE]
+            mail_log.time = details[COL_LOG_TIME]
+            mail_log.id_self = details[COL_LOG_KEY_ID]
+            mail_log.another_information = " ".join(details[COL_LOG_FLAG:])
 
         data.append(mail_log)
     file.close()
@@ -52,5 +52,5 @@ def parse_log_file(file_name: str, order: list):
 
 if __name__ == "__main__":
     result = parse_log_file('out', order = ["date", "time", "id", "flag", "email", "another information"])
-    database.set_values_from_mail_log(result)
+    # database.set_values_from_mail_log(result)
     test = 'ffff'
