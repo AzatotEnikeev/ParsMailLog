d6e9404d24994c8a86f4eeb723dcbc024ab924d4
AzatE <marateni@gmail.com>
2024-05-31 09:04:52 +0500

    Добавляет проверку ограничения на кол-во записей

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from backend.parse_log import parse_log_file
+from backend.constants import COL_SHOW_TEXT, COL_SHOW_TIME
 from sql.database import Session
 from sql.database_func import select_values_from_tables, set_values_from_mail_log
 
 # models.Base.metadata.create_all(bind=engine)
+MAX_COUNT_LENGTH_RECORDS = 100
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+
+def is_constrain_length_for_records(records: list) -> bool:
+    """
+    Проверяет на удовлетв размерам
+    :param records:
+    :return:
+    """
+    if len(records) > MAX_COUNT_LENGTH_RECORDS:
+        return False
+    else:
+        return True
 
 
 # Dependency
@@ -45,9 +59,19 @@ async def index(
 ):
     if mail_name != "default":
         records = select_values_from_tables(db, mail_name)
+        if not is_constrain_length_for_records(records):
+            records = [{COL_SHOW_TIME: f'Количество записей больше ограничения {MAX_COUNT_LENGTH_RECORDS}',
+                       COL_SHOW_TEXT: ''}]
+            print(records)
+
         context = {"request": request, "records": records}
         if hx_request:
             return templates.TemplateResponse("table.html", context)
+
+
+
+
+
     records = []
     context = {"request": request, "records": records}
     return templates.TemplateResponse(name="index.html", context=context)
